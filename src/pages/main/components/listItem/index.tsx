ff619dacd1b2aa3c56f7264ccf14806d4f2c4266
yuriivolchak@busmarket.ua <VV131202vv01>
2021-11-12 14:51:29 +0200

    fix filter and list items

--- src/pages/main/components/listItem/index.tsx
+++ src/pages/main/components/listItem/index.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, {} from "react";
 import * as _ from 'lodash'
-
-
+import {DataRow} from "../../../../elements/dataRow";
+// import s from ''
 import './_styles.scss'
 
 type ListItemT = React.FC<{
@@ -10,18 +10,37 @@ type ListItemT = React.FC<{
 
 
 }>
-const ListItem:ListItemT  = ({item,i})=>{
 
-    return <>
-        <div className="item_wrapper" key={i}>
-            <h3>{item?.name?.english}</h3>
+const ListItem: ListItemT = ({item}) => {
+    // const {key} = item
+
+    const {base} = item
+    let name;
+    if(item&&item.name){
+        name=item.name.english
+    }
+    const type = item.type
+    const modifier = item.id<10?'00':item.id<100?'0':''
+    const imgUrl = `./../../../../assets/thumbnails/${modifier}${item.id||0}.png`
+    return (
+        <div className="item_wrapper" key={item.id}>
+            <h3>{name}</h3>
             <div className="item_img">
-                <img src={'url'} alt="'photo"/>
+                <img src={imgUrl} alt="'photo"/>
+            </div>
+            <div className="item_info_wrapper">
+                <DataRow title={'type'} value={type}/>
             </div>
 
+            {_.map(_.keys(base),(prop,i)=>{
+                return (
+                    <div className="item_info_wrapper" key={i}>
+                        <DataRow title={prop} value={base[prop]}/>
+                    </div>
+                )
+            })}
 
         </div>
-
-    </>
+    )
 }
 export default ListItem
--- src/pages/main/store/services.ts
+++ src/pages/main/store/services.ts
@@ -1,12 +1,18 @@
 import * as _ from "lodash";
 
-export const someService=()=>{
+export const someService = () => {
     console.log('my test service')
 }
 export const filterBy = (data: Array<object>, key: any, secondKey: string, first: string, second: string,) => {
-
     return _.filter([...data], (el: any) => {
-        return _.includes(el[key]['english']?.toString().toLowerCase(), first.toLowerCase())
+
+        if (el.name) {
+             el.name=el.name.engllish
+        }
+        else{
+            el.name =''
+        }
+        return _.includes(el?.name?.toString().toLowerCase(), first.toLowerCase())
             && (second ? el[secondKey].includes(second) : true)
     })
 }
